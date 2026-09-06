aeaadbf017d2e9d09397303e333a98c33ad85637
Brent Anderson <bma42@students.uwf.edu>
2026-09-06 13:10:40 -0500

    testing for getArguments and addArgument functions with destructor tests

--- myshell.cpp
+++ myshell.cpp
@@ -1,10 +1,23 @@
 #include <cstdlib>
+#include <cstdio>
 #include "param.hpp"
 
 int main(int argc, char* argv[]) {
 	Param *testParam = new Param();
 
+	for (int i = 0; i < argc; i++) 
+                testParam->addArgument(argv[i]);
+
 	testParam->printParams();
+
+	char **testGetArguments = testParam->getArguments();
+
+	for (int i = 0; i < argc; i++) 
+		fprintf(stdout, "%s\n", testGetArguments[i]);
+
+	delete[] testGetArguments;
+
+	delete testParam;
 
 	return EXIT_SUCCESS;
 }
