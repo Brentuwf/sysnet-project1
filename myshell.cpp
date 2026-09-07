3f8d6630a218be6c1d6c9043cf4f5ec89ec54edf
Brent Anderson <bma42@students.uwf.edu>
2026-09-07 12:25:47 -0500

    removed output test for getArguments causing seg fault, added test for exception handling

--- myshell.cpp
+++ myshell.cpp
@@ -1,19 +1,25 @@
 #include <cstdlib>
-#include <cstdio>
+#include <iostream>
 #include "param.hpp"
 
 int main(int argc, char* argv[]) {
 	Param *testParam = new Param();
 
-	for (int i = 0; i < argc; i++) 
-                testParam->addArgument(argv[i]);
+	for (int i = 0; i < argc; i++) { 
+		try {
+                	testParam->addArgument(argv[i]);
+		}
+		catch (std::invalid_argument& message) {
+			std::cerr << message.what() << std::endl;
+		}
+		catch (std::out_of_range& message) {
+			std::cerr << message.what() << std::endl;
+		}
+	}
 
 	testParam->printParams();
 
 	char **testGetArguments = testParam->getArguments();
-
-	for (int i = 0; i < argc; i++) 
-		fprintf(stdout, "%s\n", testGetArguments[i]);
 
 	delete[] testGetArguments;
 
