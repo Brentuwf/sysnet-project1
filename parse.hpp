6906a17431edea7b82d0acbe1e36f24147f404f3
Brent Anderson <bma42@students.uwf.edu>
2026-09-08 12:05:38 -0500

    added decalration for functions and and variables for parse class, class life cycle Parse() -> tokenizer() -> isValidInput() -> tokensToParam -> getParameters() -> ~Parse(). destructor is responsible for cleaning up memory for Param object it creates after parsing an input. private helper function pass pointer for input to each other

--- parse.hpp
+++ parse.hpp
@@ -3,16 +3,21 @@
 
 class Parse {
 	private:
+		Param *commandParameters;
 		/**
 		 * Validates input after it has been passed to the tokenizer
 		 * Disallows no space between input and output redirect symbols
 		 * @param char *input, character pointer for input string, assumes non empty string is passed
 		 * @throw invalid_argument exception if regex returns false
 		 */
-		void isValidInput(char *input) const;
+		void isValidInput(char *tokenizedInput) const;
+		char *tokenizer(char *input);
+		void tokensToParam(char *tokenizedInput)
 
 	public:
-
+		Parse(char *input);
+		~Parse();
+		Param *getParameters() const;
 
 
 };
