9cfd4d17df17f9f00ded74e189e9f853b410c321
Brent Anderson <bma42@students.uwf.edu>
2026-09-10 16:04:59 -0500

    removed smoke testing for param class

--- myshell.cpp
+++ myshell.cpp
@@ -70,29 +70,6 @@ int main(int argc, char **argv) {
 		parameters = nullptr;
 
 	} while(true);
-
-	/* test below remove before submission */
-	Param *testParam = new Param();
-
-	for (int i = 0; i < argc; i++) {
-		try {
-			testParam->addArgument(argv[i]);
-		}
-		catch (std::invalid_argument& message) {
-			std::cerr << message.what() << std::endl;
-		}
-		catch (std::out_of_range& message) {
-			std::cerr << message.what() << std::endl;
-		}
-	}
-
-	testParam->printParams();
-
-	char **testGetArguments = testParam->getArguments();
-
-	delete[] testGetArguments;
-
-	delete testParam;
-
+	
 	return EXIT_SUCCESS;
 }
